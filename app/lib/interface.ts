07e6692fa6aa8c8ebc72119b96214304e20e9141
Lucas Barragana <lucasbarraga@gmail.com>
2024-04-23 22:20:03 -0300

    add vercelgit

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -11,3 +11,41 @@ export interface simpleBlogCard {
     content: any;
     titleImage: any;
   }
+
+  export interface simpleProjectCard{
+    title: string;
+    description: string;
+    currentSlug: string;
+    titleImage: any;    
+    githubUrl: string;
+    website: string;
+    tecsImage1: any;
+    tecsImage2: any;
+    tecsImage3: any;
+    tecsImage4: any;
+    tecsImage5: any;
+    tecsImage6: any;
+    photo1: any;
+    photo2: any;
+    photo3: any;
+    photo4: any;
+    photo5: any;
+    photo6: any;
+  }
+
+  export interface fullProject {
+    currentSlug: string;
+    title: string;
+    description: string;
+    titleImage: any;
+    titleImageUrl: string;
+    githubUrl: string;
+    website: string;
+    tecsImage1: any;
+    tecsImage2: any;
+    tecsImage3: any;
+    tecsImage4: any;
+    tecsImage5: any;
+    tecsImage6: any;
+    content: any;
+  }
